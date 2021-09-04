eb817221f6826b4a1e8e09405dead263c2903d89
Blue Ye <yejianfengblue@163.com>
2021-09-04 15:03:00 +0800

    Emit event from button to header

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -11,12 +11,14 @@ export class ButtonComponent implements OnInit {
 
   @Input() color = "blue";
 
+  @Output() btnClick = new EventEmitter();
+
   constructor() { }
 
   ngOnInit(): void {}
 
   onClick() {
-    console.log('Add')
+    this.btnClick.emit();
   }
 
 }
