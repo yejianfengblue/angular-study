4d00c1fe571d0af5670198c9e4f60832a3a0bb4b
Blue Ye <yejianfengblue@163.com>
2021-09-06 23:40:01 +0800

    Make tasks subscribe to rxjs observable

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { Task } from '../Task';
 import { TASKS } from '../mock-tasks';
 
@@ -9,8 +10,8 @@ export class TaskService {
 
   constructor() { }
 
-  getTasks(): Task[] {
+  getTasks(): Observable<Task[]> {
 
-    return TASKS;
+    return of(TASKS);
   }
 }
