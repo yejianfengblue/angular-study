27f3b589e563ce72d1f16636c42e88c19c21f5b8
Blue Ye <yejianfengblue@163.com>
2021-09-06 23:27:07 +0800

    Add an 'x' button

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Task } from '../../Task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+
 
 @Component({
   selector: 'app-task-item',
@@ -9,6 +12,8 @@ import { Task } from '../../Task';
 export class TaskItemComponent implements OnInit {
 
   @Input() task: Task;
+
+  faTimes = faTimes;
 
   constructor() { }
 
