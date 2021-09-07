6387aa2c8591e856f70d0064c1336062b7403898
Blue Ye <yejianfengblue@163.com>
2021-09-07 13:50:28 +0800

    Add router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -9,6 +10,17 @@ import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AboutComponent } from './components/about/about.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: TasksComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  }
+]
 
 @NgModule({
   declarations: [
@@ -22,7 +34,8 @@ import { AboutComponent } from './components/about/about.component';
   imports: [
     BrowserModule,
     FontAwesomeModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
